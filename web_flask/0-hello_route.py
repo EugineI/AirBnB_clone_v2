72679364a4409e40dec02fde066ef853e03062eb
EugineI <eugineindimuli@gmail.com>
2025-02-07 14:46:27 +0300

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Start a flask framework"""
+#Start a flask framework#
 from flask import Flask
 app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    """this is the index function"""
+    #this is the index function#
     return"Hello HBNB!"
 
 if __name__ == "__main__":
