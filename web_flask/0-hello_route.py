48de715a5c0ad9fd5014a4e36d70f44728df77d7
EugineI <eugineindimuli@gmail.com>
2025-02-07 14:35:56 +0300

    first web framework

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Start a flask framework"""
+from flask import Flask
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    return"Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
